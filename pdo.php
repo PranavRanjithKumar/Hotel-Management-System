75740daaf35088733f044ca14191ce6f6e1749bc
PranavRanjithKumar <66897652+PranavRanjithKumar@users.noreply.github.com>
2021-04-26 22:44:24 +0530

    Security improvements done

--- pdo.php
+++ pdo.php
@@ -1,3 +1,3 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=hotelbooking', 'pranav', 'pranav21');
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=hotelbooking', //'username', 'password');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
